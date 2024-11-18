dda0f956762e67450def513d0ec6bb8d991b1521
Juan Diego ORdoñe <juordonezl@unal.edu.co>
2024-11-18 17:32:13 -0500

    main

--- Laboratorio #2/main.py
+++ Laboratorio #2/main.py
@@ -1,13 +1,6 @@
-from direccion import Direccion
+from direccion import *
 class Main:
-    @staticmethod
-    def run():
-        # Crear una instancia de la clase Direccion
+  
+    if __name__ == "__main__":
         direccion1 = Direccion("Calle 54A", "30-01", "Boston", "Medellín", "Edificio Central", "101")
-        
-        # Mostrar los atributos del objeto direccion1
         print(direccion1)
-
-# Código principal que llama a la clase Main
-if __name__ == "__main__":
-    Main.run()
